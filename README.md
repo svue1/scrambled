b2643db16dcb2fa2a57e05166ada229794f28afb
Aberdeen Morrow <amorrow@macalester.edu>
2021-12-10 10:52:20 -0600

    tweaks

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # scrambled
 COMP446 Final and Capstone
+
+To run locally: 
+1. Pull this project. 
+2. Create and activate a virtual environment on your machine in the same directory as this project.
+3. With your virtual environment activated, install requirements from requirements.txt.
--- static/app.js
+++ static/app.js
@@ -1,6 +1,17 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
 
+// Makes navbar responsive to window size
+function myFunction() {
+  var x = document.getElementById("top-nav");
+  if (x.className === "nav-bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-bar";
+  }
+}
+
+// Controls opening and closing accordions (i.e. on user profile page)
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
@@ -16,3 +27,4 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
